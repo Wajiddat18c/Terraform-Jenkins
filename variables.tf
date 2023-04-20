415bf419a2d0c4be88803cf72486f983e5dfe25d
waja <waja@netcompany.com>
2023-04-20 09:28:35 +0200

    Added default values

--- variables.tf
+++ variables.tf
@@ -6,21 +6,26 @@ variable "endpoint" {
 variable "partitionname" {
   type        = string
   description = "Partition name for the new partition"
+  default = ""
 }
 
 variable "maxbandwidth" {
   type        = number
   description = "Max bandwidth for the new partition"
+  default = 0
 }
 variable "maxconn" {
   type        = string
   description = "Max connections for the new partition"
+  default = "0"
 }
 variable "maxmemlimit" {
   type        = string
   description = "Max memory limit for the new partition"
+  default = "0"
 }
 variable "vlan" {
   type        = string
   description = "VLAN of the new partition"
+  default = "0"
 }
